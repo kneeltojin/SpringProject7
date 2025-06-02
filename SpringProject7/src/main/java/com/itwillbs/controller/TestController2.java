d140fca863ea2f3cb3c3c10e5b245603b1759bb4
kneeltojin <github hgcom@naver.com>
2025-06-02 10:31:59 +0900

    두번째 컨트롤러 생성

---
+++ SpringProject7/src/main/java/com/itwillbs/controller/TestController2.java
@@ -0,0 +1,16 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController2 {
+
+	@RequestMapping(value="/test2",method=RequestMethod.GET)
+	public void test2() {
+		
+	}
+	
+	
+}
